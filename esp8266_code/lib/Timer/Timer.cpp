d6fde63969a74bcf60a91dbc7a6732eaeb17c7a2
Brent van de Pol <binksydinks@protonmail.com>
2023-03-30 16:47:44 +0200

    ESP8266: Created Timer Class

---
+++ esp8266_code/lib/Timer/Timer.cpp
@@ -0,0 +1,45 @@
+#include "Timer.hpp"
+
+#include <Arduino.h>
+
+Timer::Timer()
+{
+    startTime = 0;
+    delayInMs = 0;
+    started = false;
+    finished = false;
+}
+
+void Timer::Start(uint32_t delayInMs)
+{
+    started = true;
+    this->delayInMs = delayInMs;
+    startTime = millis();
+}
+
+void Timer::Run()
+{
+    if(!started)
+        return;
+    
+    uint32_t currentTime = millis();
+
+    if((currentTime - startTime) >= delayInMs)
+    {
+        started = false;
+        finished = true;
+    }
+}
+
+void Timer::Reset()
+{
+    startTime = 0;
+    delayInMs = 0;
+    started = false;
+    finished = false;
+}
+
+bool Timer::Finished() const
+{
+    return finished;
+}
---
+++ esp8266_code/lib/Timer/Timer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdint.h>
+
+class Timer
+{
+private:
+    uint32_t startTime;
+    uint32_t delayInMs;
+    bool started;
+    bool finished;
+
+public:
+    Timer();
+    void Start(uint32_t delayInMs);
+    void Run();
+    void Reset();
+    bool Finished() const;
+};
