28f86eaf80c05090ed449f43311b11b337d9cf52
Brent van de Pol <binksydinks@protonmail.com>
2023-06-02 14:07:44 +0200

    Fixed mistake in MatrixTimer.

--- arduino_code/lib/MatrixTimer/MatrixTimer.hpp
+++ arduino_code/lib/MatrixTimer/MatrixTimer.hpp
@@ -11,7 +11,7 @@ class MatrixTimer
         bool started;
         bool finished;
     public:
-        MatrixTimer(unsigned long(*time)(), uint32_t cycleTime);
+        MatrixTimer(unsigned long(*time)());
         void Start(uint32_t cycleTime);
         void Run();
         bool IsStarted() const;
